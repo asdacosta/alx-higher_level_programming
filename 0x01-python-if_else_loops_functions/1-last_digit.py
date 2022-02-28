c9b077f24e299ce664928e60b49f77bd62456744
asdacosta <dacostasilvanus@gmail.com>
2022-02-28 08:29:20 +0000

    Udpated: Changed approach to print last digit of neg

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
+
 print("Last digit of {} is ".format(number), end=" ")
-if ((number % 10) > 5):
-    print("{} and is greater than 5".format(number % 10))
-elif ((number % 10) == 0):
-    print("{} and is 0".format(number % 10))
-elif ((number % 10) < 6 and not((number % 10) == 0)):
-    print("{} and is less than 6 and not 0".format(number % 10))
+if (number >= 0):
+    last = number % 10
+else:
+    last = number % -10
+
+if (last > 5):
+    print("{} and is greater than 5".format(last))
+elif (last == 0):
+    print("{} and is 0".format(last))
+else:
+    print("{} and is less than 6 and not 0".format(last))
 
