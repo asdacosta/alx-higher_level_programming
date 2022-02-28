06b4f03f1942b16979191d118cceaf87c0ad9489
asdacosta <dacostasilvanus@gmail.com>
2022-02-28 09:58:18 +0000

    Updated: Print all num as double digits

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 for num in range(0, 100):
     if (num < 10):
-        print("{:0d}, ".format(num), end="")
-    print("{:d}, ".format(num), end="")
+        print("0{:d}, ".format(num), end="")
+    elif:
+        print("{:d}, ".format(num), end="")
