3e5631d4a2c290b1bf0fd9ad3f0d83b68a77e73f
asdacosta <dacostasilvanus@gmail.com>
2022-02-28 00:10:24 +0000

    Updated: Using diff method print last digit of random

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-1000, 1000)
+print("Last digit of {} is ".format(number), end=" ")
+if (number >= 0):
+    print("{} and is ".format(number % 10), end=" ")
+else:
+    print("{} and is less than 6 and not 0".format(number % -10))
+
 if ((number % 10) > 5):
-    print("Last digit of {} is {} and is greater than 5".format(number, number%10))
+    print("greater 5")
 elif ((number % 10) == 0):
-    print("Last digit of {} is {} and is 0".format(number, number%10))
+    print("0")
 elif ((number % 10) < 6 and not(number % 10) == 0):
-    print("Last digit of {} is {} and is less than 6 and not 0".format(number, number%10))
+    print("{} and is less than 6 and not 0".format(number % 10))
 
