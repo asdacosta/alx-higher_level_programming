549a6bea370990b869a1b81728f5bfafd1111044
asdacosta <dacostasilvanus@gmail.com>
2022-02-28 11:11:37 +0000

    Updated: Clear all new lines

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for num in range(0, 100):
-    if (not(num) = 99):
-        print("{:02d}, ".format(num))
+    if (not(num) == 99):
+        print("{:02d}, ".format(num), end="")
     else:
         print("{:02d}".format(num))
