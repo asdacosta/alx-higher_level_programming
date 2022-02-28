451b1d36f6d93542978edf5ebbde9bb278da3376
asdacosta <dacostasilvanus@gmail.com>
2022-02-28 20:11:10 +0000

    Created: Checks for lowercase character

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 def islower(c):
-    if ord(c) >= 97 and ord(c) <= 122:
-        return (True)
-    else:
-        return (False)
+    c = 65
+    while (c <= 122):
+        for c in range(65, 90):
+            print("{:02c} is upper".format(c))
+        for c in range(97, 123):
+            print("{:02c} is lower".format(c))
+        else:
+            continue
+
+        c += 1
