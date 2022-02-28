6dc65d46db7ac9a327e90f8437a640e7d4735fc1
asdacosta <dacostasilvanus@gmail.com>
2022-02-28 09:34:45 +0000

    Created: Prints numbers in decimal and dexd

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,3 +1,19 @@
 #!/usr/bin/python3
-for num in range(0, 99):
-        print("{0:d} = 0x{0:x}".format(num))
+for num in range(0, 98):
+    print("{} = 0x".format(num), end="")
+    if (num < 10):
+        print("{}".format(num))
+    elif (num == 10):
+        print("{:c}".format(97))
+    elif (num == 11):
+        print("{:c}".format(98))
+    elif (num == 12):
+        print("{:c}".format(99))
+    elif (num == 13):
+        print("{:c}".format(100))
+    elif (num == 14):
+        print("{:c}".format(101))
+    elif (num == 15):
+        print("{:c}".format(102))
+    elif (num == 16):
+        print("{}".format(num - 6))
