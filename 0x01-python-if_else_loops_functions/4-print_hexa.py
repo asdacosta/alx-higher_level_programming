f93e28b9490a887e9fce6a14baea959b075f0293
asdacosta <dacostasilvanus@gmail.com>
2022-02-28 09:38:33 +0000

    Updated: Print latter numbers

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -15,5 +15,5 @@ for num in range(0, 98):
         print("{:c}".format(101))
     elif (num == 15):
         print("{:c}".format(102))
-    elif (num == 16):
-        print("{}".format(num - 6))
+    elif (num >= 16):
+        print("{}".format(num = num - (num % 10))
