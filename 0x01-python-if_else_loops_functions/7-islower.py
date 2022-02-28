1e9011f93a09dcd39e0d2c027dbbbf1c37bd866f
asdacosta <dacostasilvanus@gmail.com>
2022-02-28 20:23:09 +0000

    Updated: Printing the unicode char of numbers

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
-    c = 65
-    while (c <= 122):
-        for c in range(65, 90):
-            print("{:02c} is upper".format(c))
-        for c in range(97, 123):
-            print("{:02c} is lower".format(c))
-        else:
-            continue
-
-        c += 1
+    for ord(c) in range(97, 122):
+        return(True)
+    else:
+        return(False)
